cde910843de6abc506eeffbf7b47b84cf10061bb
Syed Moiz Ur Rehman <smrehman@proton.me>
2025-03-19 19:15:57 +0000

    Update `method_has_no_args` to ignore parameters with default arguments

--- django/utils/inspect.py
+++ django/utils/inspect.py
@@ -68,7 +68,13 @@ def func_accepts_var_args(func):
 
 def method_has_no_args(meth):
     """Return True if a method only accepts 'self'."""
-    count = len([p for p in _get_callable_parameters(meth) if p.kind in ARG_KINDS])
+    count = len(
+        [
+            p
+            for p in _get_callable_parameters(meth)
+            if p.kind in ARG_KINDS and p.default == inspect.Parameter.empty
+        ]
+    )
     return count == 0 if inspect.ismethod(meth) else count == 1
 
 
